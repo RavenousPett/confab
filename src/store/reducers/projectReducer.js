ea29e7a6c6beb495c1a6e38b25906a948eeea487
Richard <richard@tlgd.co.uk>
2019-01-28 18:01:38 +0000

    Connect data from store to the dashobard and do a loop in project list

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+    'projects': [
+        {id: '1', title: 'Book car', 'content': 'Ideally a pick up truck'},
+        {id: '2', title: 'Book flights', 'content': 'Please dont sit me next to jed'},
+        {id: '3', title: 'Book TWE', 'content': 'The Whole Enchilada!!'}
+    ]
+};
 
 const projectReducer = (state = initState, action) => {
     return state;
