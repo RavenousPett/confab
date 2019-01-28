6cef591eb21dc195d80f14b928359c1546001313
Richard <richard@tlgd.co.uk>
2019-01-28 16:46:26 +0000

    Typo on button|

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -49,7 +49,7 @@ export class SignUp extends Component {
                     </div>
 
                     <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0">Login</button>
+                        <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
                     </div>
                 
                 </form>
