e49cfcb9e338c34bc364d8de45368553d29b3b9e
Richard <richard@tlgd.co.uk>
2019-01-28 19:10:06 +0000

    Installed firebase npm package and set up firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyARs9juFAl8ntl0kXZB3vDZoLziXDnzRos",
+    authDomain: "confab-b8a31.firebaseapp.com",
+    databaseURL: "https://confab-b8a31.firebaseio.com",
+    projectId: "confab-b8a31",
+    storageBucket: "confab-b8a31.appspot.com",
+    messagingSenderId: "749221437257"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
