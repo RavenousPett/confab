4007e8766407f1720d245c4ba580aa64785a3d8e
Richard <richard@tlgd.co.uk>
2019-01-28 20:48:49 +0000

    Adding store enhancers so we can connect to fire base in our redux actions file

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,5 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFireStore }) => {
         // make async call...
 
         dispatch({
