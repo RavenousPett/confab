2cb5152b2505af50230256760855f5c068284b7c
Richard <richard@tlgd.co.uk>
2019-01-29 12:21:47 +0000

    Log a user out, had to make a tag a button othewise got warning so should fix styles

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { signOut } from '../../store/actions/authActions';
 
-const SignedInLinks = () => {
+const SignedInLinks = (props) => {
     return (
         <ul className="right">
             <li><NavLink to="/create">New Project</NavLink></li>
-            <li><NavLink to="/">Log Out</NavLink></li>
+            <li><button onClick={props.signOut}>Log Out</button></li>
             <li><NavLink to="/" className="btn btn-floating pink lighten-1">RP</NavLink></li>
         </ul>
     );
 }
 
-export default SignedInLinks;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signOut: () => dispatch(signOut())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(SignedInLinks);
