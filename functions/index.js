0c1d1939340b763c76eeae529c81b64a0af8a49f
Richard <richard@tlgd.co.uk>
2019-01-29 19:34:47 +0000

    Fixed projectCreated function

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,7 @@ const createNotification = (notification => {
 });
 
 exports.projectCreated = functions.firestore.document('projects/{projectId}').onCreate(doc => {
-    const project = doc.data;
+    const project = doc.data();
 
     const notification = {
         content: 'Added a new project',
