2d7f0578026334bc56968a0bf35fbeabbf838c95
Richard <richard@tlgd.co.uk>
2019-01-29 19:49:21 +0000

    Ran npm run build, copied to contents from build directory into dist directory

---
+++ dist/precache-manifest.0da4250d98ddfb17d3e910fca8647f27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d0357df2f5e97a09afc",
+    "url": "/static/css/main.690f3276.chunk.css"
+  },
+  {
+    "revision": "0d0357df2f5e97a09afc",
+    "url": "/static/js/main.0d0357df.chunk.js"
+  },
+  {
+    "revision": "60311e3e6ccaab5a70fe",
+    "url": "/static/js/1.60311e3e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e12caab3cda8bac691bf945f1a168162",
+    "url": "/index.html"
+  }
+];
