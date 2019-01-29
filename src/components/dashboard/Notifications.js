4bbc73c60719978d75703738235259308547d4c3
Richard <richard@tlgd.co.uk>
2019-01-29 16:56:33 +0000

    Beefing out the notification component a bit

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
-const Nofifications = () => {
+const Nofifications = (props) => {
     return (
-        <div><p>Nofifications</p></div>
+        <div className="section">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    
+                    <span className="card-title">Nofifications</span>
+
+                    <ul className="notifications">
+                        <li>Nofification</li>
+                        <li>Nofification</li>
+                        <li>Nofification</li>
+                        <li>Nofification</li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
     );
 }
 
