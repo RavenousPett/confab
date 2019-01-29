8f363183f062d35f754782c8c3fbd4b97170b0bd
Richard <richard@tlgd.co.uk>
2019-01-29 17:53:05 +0000

    Added new function for user sign up

--- functions/index.js
+++ functions/index.js
@@ -11,7 +11,7 @@ const createNotification = (notification => {
     return admin.firestore().collection('notifications')
         .add(notification)
         .then(doc => console.log('notification added', doc));
-})
+});
 
 exports.projectCreated = functions.firestore.document('projects/{projectId}').onCreate(doc => {
     const project = doc.data;
@@ -23,4 +23,19 @@ exports.projectCreated = functions.firestore.document('projects/{projectId}').on
     }
 
     return createNotification(notification);
-})
+});
+
+exports.userJoined = functions.auth.user().onCreate(user => {
+    return admin.firestore().collection('users')
+        .doc(user.uid).get().then(doc => {
+            const newUser = doc.data();
+
+            const notification = {
+                content: 'Joined the party',
+                user: `${newUser.firstName} ${newUser.lastName}`,
+                time: admin.firestore.FieldValue.serverTimestamp()
+            }
+
+            return createNotification(notification);
+        });
+});
